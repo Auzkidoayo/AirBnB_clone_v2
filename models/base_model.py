25bc94b92df18b53717721abc2a0752b847c7803
auzkidoayo <adedunniayokunle@gmail.com>
2024-01-20 11:51:37 -0800

    lcy

---
+++ models/base_model.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python3
+"""This module defines a base class for all models in our hbnb clone"""
+import uuid
+from datetime import datetime
+import models
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+
+class BaseModel:
+    """A base class for all hbnb models"""
+    id = Column(String(60), unique=True, nullable=False, primary_key=True)
+    created_at = Column("created_at", DateTime, nullable=False,
+                        default=(datetime.utcnow()))
+    updated_at = Column("updated_at", DateTime, nullable=False,
+                        default=(datetime.utcnow()))
+
+    def __init__(self, *args, **kwargs):
+
+        """Instantiation of base model class
+        Args:
+            args: it won't be used
+            kwargs: arguments for the constructor of the BaseModel
+        Attributes:
+            id: unique id generated
+            created_at: creation date
+            updated_at: updated date
+        """
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
+        if len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if key == "updated_at" or key == "created_at":
+                    self.__dict__[key] = datetime.strptime(value, time)
+                else:
+                    self.__dict__[key] = value
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+            models.storage.new(self)
+            models.storage.save()
+
+    def __repr__(self):
+        """return a string representaion
+        """
+        return self.__str__()
+
+    def __str__(self):
+        """Returns a string representation of the Base instance"""
+        d = self.__dict__.copy()
+        d.pop("_sa_instance_state", None)
+        return "[{}] ({}) {}".format(type(self).__name__, self.id, d)
+
+    def save(self):
+        """Updates updated_at with current time when instance is changed"""
+        from models import storage
+        self.updated_at = datetime.now()
+        models.storage.new(self)
+        models.storage.save()
+
+    def to_dict(self):
+        """Convert instance into dict format"""
+        dictionary = dict(self.__dict__)
+        dictionary["__class__"] = str(type(self).__name__)
+        dictionary["created_at"] = self.created_at.isoformat()
+        dictionary["updated_at"] = self.updated_at.isoformat()
+        dictionary.pop("_sa_instance_state", None)
+        return dictionary
+
+    def delete(self):
+        """ delete object
+        """
+        models.storage.delete(self)
---
+++ models/place.py
@@ -0,0 +1,110 @@
+#!/usr/bin/python3
+""" Place Module for HBNB project """
+import models
+import os
+from os import getenv
+from models.base_model import Base, BaseModel
+from models.review import Review
+from models.amenity import Amenity
+import sqlalchemy
+from sqlalchemy import Table, Column, Float, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
+
+
+place_amenity = Table('place_amenity',
+                      Base.metadata,
+                      Column(
+                             'place_id', String(60), ForeignKey('places.id'),
+                             nullable=False, primary_key=True
+                      ),
+                      Column(
+                             'amenity_id', String(60),
+                             ForeignKey('amenities.id'),
+                             nullable=False, primary_key=True
+                      ))
+
+
+class Place(BaseModel, Base):
+    """This is the class for Place
+    Attributes:
+        __tablename__ : represents the table name
+        city_id: city id
+        user_id: user id
+        name: name input
+        description: string of description
+        number_rooms: number of room in int
+        number_bathrooms: number of bathrooms in int
+        max_guest: maximum guest in int
+        price_by_night:: pice for a staying in int
+        latitude: latitude in flaot
+        longitude: longitude in float
+        amenity_ids: list of Amenity ids
+    """
+
+    __tablename__ = "places"
+
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+        name = Column(String(128), nullable=False)
+        description = Column(String(1024), nullable=True)
+        number_rooms = Column(Integer, nullable=False, default=0)
+        number_bathrooms = Column(Integer, nullable=False, default=0)
+        max_guest = Column(Integer, nullable=False, default=0)
+        price_by_night = Column(Integer, nullable=False, default=0)
+        latitude = Column(Float, nullable=True)
+        longitude = Column(Float, nullable=True)
+        reviews = relationship("Review", passive_deletes=True, backref="place")
+        amenities = relationship(
+                                 "Amenity", secondary=place_amenity,
+                                 back_populates="place_amenities",
+                                 viewonly=False)
+    else:
+        city_id = ""
+        user_id = ""
+        name = ""
+        description = ""
+        number_rooms = 0
+        number_bathrooms = 0
+        max_guest = 0
+        price_by_night = 0
+        latitude = 0.0
+        longitude = 0.0
+        amenity_ids = []
+
+        @property
+        def reviews(self):
+            """
+            Returns the list of Review instances
+            """
+            review_dict = models.storage.all('Review')
+            review_list = []
+            for rev in review_dict.values():
+                if rev.place_id == self.id:
+                    review_list.append(rev)
+            return rev
+
+        @property
+        def amenities(self):
+            """
+            Getter
+            Returns the list of Amenity instances based on the attribute
+            amenity_ids that contains all Amenity.id linked to the Place
+            """
+            obj_list = []
+            objs = models.storage.all('Amenity')
+            for amen in objs.value():
+                if amen.id in amenities_ids:
+                    obj_list.append(amen)
+            return obj_list
+
+        @amenities.setter
+        def amenities(self, obj):
+            """
+            Setter
+            Handles append method for adding an Amenity.id
+            to the attribute amenity_ids
+            """
+            if isinstance(obj, Amenity):
+                if self.id == obj.place_id:
+                    self.amenity_ids.append(obj.id)

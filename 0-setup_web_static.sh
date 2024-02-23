0ec929d58d28ec927db921890124c8dd972f794f
auzkidoayo <adedunniayokunle@gmail.com>
2024-02-23 11:14:45 -0800

    fjjd

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,19 +1,18 @@
 #!/usr/bin/env bash
-# Install nginx and prepare web server for static code deloyment
-
-# nginx installation
-sudo apt-get update
-sudo apt-get -y install nginx
-
-# create necessary directories
-sudo mkdir -p /data/web_static/releases/test
-sudo mkdir -p /data/web_static/shared
-
-# create index.html and symlink
-sudo echo -e "Hello World" | sudo tee "/data/web_static/releases/test/index.html"
-sudo ln -sf "/data/web_static/releases/test/" "/data/web_static/current"
-
-# change owners and alter config file, then restart nginx
-sudo chown -R ubuntu:ubuntu "/data/"
-sudo sed -i "29i \\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}" "/etc/nginx/sites-available/default"
+# Setup a web servers for the deployment of web_static.
+apt update -y
+apt install -y nginx
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+echo "<!DOCTYPE html>
+<html>
+  <head>
+  </head>
+  <body>
+    <p>Nginx server test</p>
+  </body>
+</html>" | tee /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu:ubuntu /data
+sudo sed -i '39 i\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n' /etc/nginx/sites-enabled/default
 sudo service nginx restart
